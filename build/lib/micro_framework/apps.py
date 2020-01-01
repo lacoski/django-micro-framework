57f839c622a983b63239627468628ca4e4fb8f0a
thanhnb <thanh.nguyenba0611@gmail.com>
2020-01-01 17:55:56 +0700

    clear temporary directory

--- build/lib/micro_framework/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MicroFrameworkConfig(AppConfig):
-    name = 'micro_framwork'
-    verbose_name = "Django Micro framework"
--- build/lib/micro_framework/jwt_auth/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthJWTTokenConfig(AppConfig):
-    name = 'micro_framwork.jwt_auth'
-    verbose_name = "JWT Auth Token"
